587f70ca34e7db5f432888edc45480eb30b22ca2
Alex Marshall <alex@alex-HP-EliteBook-8560p.(none)>
2012-07-13 12:31:11 +1200

    Changed the source to add database handling to add insults only

--- gen/com/alex/fod/R.java
+++ gen/com/alex/fod/R.java
@@ -20,8 +20,9 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int button1=0x7f060000;
-        public static final int button2=0x7f060001;
+        public static final int button1=0x7f060001;
+        public static final int button2=0x7f060002;
+        public static final int list1=0x7f060000;
     }
     public static final class layout {
         public static final int insults=0x7f030000;
