45c5cfb220bd0c78c60dda14ddc8112f9e9be830
Alex Marshall <alex@alex-HP-EliteBook-8560p.(none)>
2012-07-17 07:52:34 +1200

    Signed-off-by: Alex Marshall <alex@alex-HP-EliteBook-8560p.(none)>

--- gen/com/alex/fod/R.java
+++ gen/com/alex/fod/R.java
@@ -31,46 +31,59 @@ public final class R {
         public static final int about_root=0x7f090000;
         public static final int about_text=0x7f090004;
         public static final int about_version=0x7f090001;
+        public static final int body=0x7f09000a;
         public static final int button_bar=0x7f090006;
+        public static final int confirm=0x7f09000b;
         public static final int image=0x7f090002;
-        public static final int list1=0x7f090008;
-        public static final int menu_about=0x7f09000d;
-        public static final int menu_manage=0x7f09000b;
-        public static final int menu_prefs=0x7f09000c;
+        public static final int list1=0x7f09000c;
+        public static final int menu1=0x7f09000f;
+        public static final int menu2=0x7f090010;
+        public static final int menu_about=0x7f090013;
+        public static final int menu_manage=0x7f090011;
+        public static final int menu_prefs=0x7f090012;
         public static final int scrollView1=0x7f090003;
-        public static final int send_insult=0x7f090009;
-        public static final int victim_jpg=0x7f09000a;
+        public static final int send_insult=0x7f09000d;
+        public static final int text1=0x7f090008;
+        public static final int title=0x7f090009;
+        public static final int victim_jpg=0x7f09000e;
     }
     public static final class layout {
         public static final int about=0x7f030000;
-        public static final int insults=0x7f030001;
-        public static final int main=0x7f030002;
+        public static final int edit_insult=0x7f030001;
+        public static final int insult_edit=0x7f030002;
+        public static final int insults=0x7f030003;
+        public static final int main=0x7f030004;
     }
     public static final class menu {
         public static final int insults_menu=0x7f080000;
         public static final int menu=0x7f080001;
     }
     public static final class string {
-        public static final int SMS_message=0x7f070013;
-        public static final int SMS_number=0x7f070012;
-        public static final int about_copyright=0x7f07000d;
-        public static final int about_text=0x7f07000e;
+        public static final int SMS_message=0x7f070014;
+        public static final int SMS_number=0x7f070013;
+        public static final int about_copyright=0x7f07000e;
+        public static final int about_text=0x7f07000f;
         public static final int app_name=0x7f070000;
         public static final int app_name_long=0x7f070001;
-        public static final int app_version=0x7f070003;
-        public static final int dialog_text=0x7f070008;
-        public static final int dialog_title=0x7f070005;
-        public static final int icon_desc=0x7f07000f;
-        public static final int menu_about=0x7f07000b;
-        public static final int menu_delete=0x7f070002;
-        public static final int menu_manage=0x7f07000a;
-        public static final int menu_prefs=0x7f07000c;
-        public static final int menu_settings=0x7f070010;
-        public static final int no_insults=0x7f070009;
-        public static final int send_insult=0x7f070006;
-        public static final int title_activity_about_dialog=0x7f070011;
-        public static final int version_desc=0x7f070004;
-        public static final int view_insult=0x7f070007;
+        public static final int app_version=0x7f070002;
+        public static final int body=0x7f070016;
+        public static final int confirm=0x7f070017;
+        public static final int dialog_text=0x7f070007;
+        public static final int dialog_title=0x7f070004;
+        public static final int editinsult=0x7f070018;
+        public static final int icon_desc=0x7f070010;
+        public static final int menu_about=0x7f07000a;
+        public static final int menu_add=0x7f07000c;
+        public static final int menu_delete=0x7f07000d;
+        public static final int menu_manage=0x7f070009;
+        public static final int menu_prefs=0x7f07000b;
+        public static final int menu_settings=0x7f070011;
+        public static final int no_insults=0x7f070008;
+        public static final int send_insult=0x7f070005;
+        public static final int title=0x7f070015;
+        public static final int title_activity_about_dialog=0x7f070012;
+        public static final int version_desc=0x7f070003;
+        public static final int view_insult=0x7f070006;
     }
     public static final class xml {
         public static final int pref_headers=0x7f040000;
--- src/com/alex/fod/Insults.java
+++ src/com/alex/fod/Insults.java
@@ -4,14 +4,17 @@ import android.app.Activity;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.support.v4.widget.SimpleCursorAdapter;
 import android.view.ContextMenu;
 import android.view.ContextMenu.ContextMenuInfo;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView.AdapterContextMenuInfo;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.SimpleCursorAdapter;
+
 
 public class Insults extends Activity {
 	private InsultsDBAdaptor mDbHelper;
@@ -20,39 +23,63 @@ public class Insults extends Activity {
 
 	private static final int ACTIVITY_CREATE = 0;
 	private static final int ACTIVITY_EDIT = 1;
+	public static ListView lv;
+	public static String[] from;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.insults);
+		lv = (ListView) findViewById(R.id.list1);
+
 		mDbHelper = new InsultsDBAdaptor(this);
 		mDbHelper.open();
+        fillData();
+    //    lv.setAdapter(new ArrayAdapter<String>(this, R.layout.insults, from));
+  //   registerForContextMenu(getViewById());
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.insults_menu, menu);
+		return true;
 	}
 
 	@SuppressWarnings("deprecation")
 	private void fillData() {
-		Cursor insultsCursor = mDbHelper.fetchAllInsults();
+		Cursor insultscursor = mDbHelper.fetchAllInsults();
 		// Get all of the notes from the database and create the item list
-		startManagingCursor(insultsCursor);
+		startManagingCursor(insultscursor);
 
 		// Create an array to specify the fields we want to display in the list
 		// (only TITLE)
-		String[] from = new String[] { InsultsDBAdaptor.KEY_TITLE };
+		from = new String[] { InsultsDBAdaptor.KEY_TITLE };
 
 		// and an array of the fields we want to bind those fields to (in this
 		// case just list1)
-		int[] to = new int[] { R.id.list1 };
-
+		int[] to = new int[] { R.id.text1 };
 		// Now create a simple cursor adapter and set it to display
 		SimpleCursorAdapter insults = new SimpleCursorAdapter(this,
-				R.layout.insults, insultsCursor, from, to);
-		setListAdapter(insults);
+		R.layout.insults, insultscursor, from, to);
+//		setListAdapter(insults);
 
-	}
 
-	private void setListAdapter(SimpleCursorAdapter insults) {
-		// TODO Auto-generated method stub
+ 	}
 
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.menu1:
+			createinsult();
+			break;
+		case R.id.menu2:
+			deleteinsult();
+			break;
+		default:
+			break;
+		}
+
+		return true;
 	}
 
 	public boolean onContextItemSelected(MenuItem item) {
@@ -67,15 +94,20 @@ public class Insults extends Activity {
 		return super.onContextItemSelected(item);
 	}
 
-	private void createNote() {
-		Intent i = new Intent(this, Insults.class);
+	private void createinsult() {
+		Intent i = new Intent(this, Insultedit.class);
 		startActivityForResult(i, ACTIVITY_CREATE);
 	}
 
+	private void deleteinsult() {
+		// Intent i = new Intent(this, Insultedit.class);
+		// startActivityForResult(i, ACTIVITY_CREATE);
+	}
+
 //	@Override
-//	protected void onListItemClick(ListView l, View v, int position, long id) {
-//		super.onListItemClick(l, v, position, id);
-//		Intent i = new Intent(this, Insults.class);
+//	protected void onitemClick(ListView l, View v, int position, long id) {
+//		super.onItemClick(l, v, position, id);
+//		Intent i = new Intent(this, Insultedit.class);
 //		i.putExtra(InsultsDBAdaptor.KEY_ROWID, id);
 //		startActivityForResult(i, ACTIVITY_EDIT);
 //	}
